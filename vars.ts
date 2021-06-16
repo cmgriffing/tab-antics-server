227de9247a5347e3bfd788320bc2bea9ac5d8803
Chris Griffing <cmgriffing@gmail.com>
2021-06-16 16:25:57 -0700

    chore: log env vars

--- vars.ts
+++ vars.ts
@@ -1,5 +1,5 @@
 import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 
 const { TWITCH_CLIENT_ID, PORT, HOSTNAME } = config();
-
+console.log({ TWITCH_CLIENT_ID, PORT, HOSTNAME });
 export { TWITCH_CLIENT_ID, PORT, HOSTNAME };
